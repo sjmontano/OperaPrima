b46f68afde6c707c35989cf62ee794d004010bcd
ReyGaRGoL <sotelomaya123@gmail.com>
2026-06-26 09:12:16 -0500

    prueba

--- prisma/migrations/20260623134318_add_tipo_comunidad/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "TipoEvento" AS ENUM ('OPEAR_PRIMA', 'COMUNIDAD');
-
--- AlterTable
-ALTER TABLE "Evento" ADD COLUMN     "disciplinas" TEXT[],
-ADD COLUMN     "link" TEXT,
-ADD COLUMN     "tipo" "TipoEvento" NOT NULL DEFAULT 'OPEAR_PRIMA';

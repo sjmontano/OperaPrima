87c28c53bdf6baf2ac6edba734057015e95f442b
ReyGaRGoL <sotelomaya123@gmail.com>
2026-06-30 17:04:46 -0500

    prueba

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,7 +1,32 @@
+import Stripe from 'stripe'
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export async function POST() {
-  console.log('Webhook recibido')
+import { stripe } from '@/lib/stripe'
+import { prisma } from '@/lib/prisma'
+
+export async function POST(req: Request) {
+  const body = await req.text()
+
+  const signature = (await headers()).get('stripe-signature')
+
+  if (!signature) {
+    return new NextResponse('No signature', {
+      status: 400,
+    })
+  }
+
+  let event: Stripe.Event
+
+  try {
+    event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!)
+  } catch (err) {
+    console.error(err)
+
+    return new NextResponse('Invalid signature', {
+      status: 400,
+    })
+  }
 
   return NextResponse.json({
     received: true,
